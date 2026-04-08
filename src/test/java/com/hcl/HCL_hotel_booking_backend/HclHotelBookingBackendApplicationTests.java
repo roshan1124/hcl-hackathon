f213aa3242b2d43e66e2661303a696f9b252fc69
Roshan <roshanvadapalli@gmail.com>
2026-04-08 12:57:37 +0530

    intial

---
+++ src/test/java/com/hcl/HCL_hotel_booking_backend/HclHotelBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.HCL_hotel_booking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HclHotelBookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
